021e95cf4113dbfa347cee30b1b800e2d3fd1b5f
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-19 17:03:38 +0200

    Adding functionality for showing the selected dashboard card content panel

--- script.js
+++ script.js
@@ -2,16 +2,34 @@
 
 giveCardsOnClicks();
 function giveCardsOnClicks(){
-    document.getElementById('MyCard').addEventListener('click', showMyPersonalPage());
-    document.getElementById('ProjectsCard').addEventListener('click', showMyProjectsPage());
-    document.getElementById('ExperienceCard').addEventListener('click', showMyExperiencePage());
-    document.getElementById('ContactCard').addEventListener('click', showMyContactPage());
+  document.getElementById('MyCard').addEventListener('click', showMyPersonalPage);
+  document.getElementById('ProjectsCard').addEventListener('click', showMyProjectsPage);
+  document.getElementById('ExperienceCard').addEventListener('click', showMyExperiencePage);
+  document.getElementById('ContactCard').addEventListener('click', showMyContactPage);
 }
 
 function showMyPersonalPage(){
-  // first we need to make our dashboard row be surrounded by a col-12
-  // then we change the col-12 to a col-3 and unhide the col-9 row
+  var dashboardPanel = document.getElementById('dashboardColumn');
+  dashboardColumn.classList.remove('col-12');
+  dashboardColumn.classList.add('col-3');
 
+  var cards = dashboardColumn.querySelectorAll('[id$="Card"]');
+  debugger;
+
+  for(let i = 0; i < cards.length; i++){
+    var cardId = cards[i].id;
+
+    if(cardId.includes('My')){
+      cards[i].parentNode.style.display = 'none';
+
+    } else{
+      cards[i].parentNode.classList.remove('col-md-4', 'col-lg-3');
+      cards[i].parentNode.classList.add('col-12');
+    }
+  }
+
+  var contentPanelColumn = document.getElementById('contentPanelColumn');
+  contentPanelColumn.removeAttribute('hidden');
 }
 
 function showMyProjectsPage(){
