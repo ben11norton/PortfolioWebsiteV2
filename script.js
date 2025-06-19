d65328bd8c9670208317ca916aa690a5926ee282
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-19 16:30:12 +0200

    setting things up for the js stuff (neovim btw feeling so good already)

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// neovim btw
 
-var myCard = document.getElementById('MyCard'); 
-// then we do something here when we get back from the surf
+giveCardsOnClicks();
+function giveCardsOnClicks(){
+    document.getElementById('MyCard').addEventListener('click', showMyPersonalPage());
+    document.getElementById('ProjectsCard').addEventListener('click', showMyProjectsPage());
+    document.getElementById('ExperienceCard').addEventListener('click', showMyExperiencePage());
+    document.getElementById('ContactCard').addEventListener('click', showMyContactPage());
+}
+
+function showMyPersonalPage(){
+  // first we need to make our dashboard row be surrounded by a col-12
+  // then we change the col-12 to a col-3 and unhide the col-9 row
+
+}
+
+function showMyProjectsPage(){
+
+}
+
+function showMyExperiencePage(){
+
+}
+
+function showMyContactPage(){
+
+}
+
